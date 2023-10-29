d2f03b3a4d3456b5eb8d132aeff8952bf44f9dc6
MSI-CNC <MSI-CNC@DESKTOP-9NSTT9D>
2023-10-29 16:43:22 +0300

    Добавьте файлы проекта.

---
+++ UseCase/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace UseCase
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            UseCase.LoadJson("Data.json");
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Items.AddRange(items:null /*UseCase.list*/);
+            listBox1.Location = new Point(34, 37);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(165, 319);
+            listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(978, 579);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+    }
+}
---
+++ UseCase/UseCase.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCase
+{
+    public class UseCase
+    {
+            
+
+            [JsonProperty("Affect")]
+            public string Affect { get; set; }
+            public string AreaOfAffect { get; set; }
+            public string Actor { get; set; }
+            public string Precondition { get; set; }
+            public string GuaranteesOfSuccess { get; set; }
+            public string Trigger { get; set; }
+            public string Description { get; set; }
+
+            public static List<UseCase> list = new List<UseCase>();
+
+            public static void LoadJson(string file)
+            {
+                using (StreamReader reader = new StreamReader(file))
+                {
+                     string json = reader.ReadToEnd();
+                     list = JsonConvert.DeserializeObject<List<UseCase>>(json);
+                };
+            }
+    }
+}
